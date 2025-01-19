7f745e9b4fe40a9ad572ba735f7d6194c743838e
SKARKVDS <114135807+SKARKVDS@users.noreply.github.com>
2025-01-19 18:21:29 +0100

    Update calendar.ts

--- layers/proposals/middleware/calendar.ts
+++ layers/proposals/middleware/calendar.ts
@@ -5,7 +5,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     const id = route.params.id;
 
     if (!id) {
-        const {data,error, status } = await useFetch('http://localhost:7777/api/proposal/accessible/' + to.params.id, {
+        const {data,error, status } = await useFetch('https://pet-connect-back-deploy.onrender.com/api/proposal/accessible/' + to.params.id, {
             onRequest({ request, options }) {
                 // Set the request headers
                 // note that this relies on ofetch >= 1.4.0 - you may need to refresh your lockfile
